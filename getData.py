0bb00580a414eb7b4b06220eecf93e42fe6aa3b4
LarryStanley <kill.xmurderer@gmail.com>
2015-08-12 19:47:56 +0800

    Update.

--- getData.py
+++ getData.py
@@ -13,8 +13,8 @@ def getData(url):
         getData(data['paging']['next'])
     else:
         allData = json.dumps(allData, sort_keys=True, indent=4, ensure_ascii=False).encode('utf-8')
-        file = open("trashData.json", 'w')
+        file = open("data.json", 'w')
         file.write(allData)
         file.close
 
-getData("https://graph.facebook.com/1460195947629865/feed?fields=actions,from,message,comments.limit(0).summary(true),likes.limit(0).summary(true)&limit=1000&access_token=CAAWnAJjuBBkBAB6ZAsyDd26F84D5iAZAopuZBv9h4GzC8RGFlwCbJOUtVoQESpBKvSCSPZC6EZC78ZBAkeHZClIkvLWY3dTZB7WZBaeybQN2bVWt8hFrqamvjwizgx66K84Wry4bDoLpWy3MV3spFMgYOPrqMG0olGb7C6Pn4r064hTi1tFODQzLi")
+getData("https://graph.facebook.com/867704779984689/feed?fields=actions,from,message,comments.limit(0).summary(true),likes.limit(0).summary(true)&limit=1000&access_token=CAAWnAJjuBBkBAB6ZAsyDd26F84D5iAZAopuZBv9h4GzC8RGFlwCbJOUtVoQESpBKvSCSPZC6EZC78ZBAkeHZClIkvLWY3dTZB7WZBaeybQN2bVWt8hFrqamvjwizgx66K84Wry4bDoLpWy3MV3spFMgYOPrqMG0olGb7C6Pn4r064hTi1tFODQzLi")
