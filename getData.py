074a3653bf92ee57057b70a31f6b6bc542c55bcc
LarryStanley <kill.xmurderer@gmail.com>
2015-08-13 17:05:55 +0800

    Update

--- getData.py
+++ getData.py
@@ -12,9 +12,17 @@ def getData(url):
     if 'paging' in data:
         getData(data['paging']['next'])
     else:
-        allData = json.dumps(allData, sort_keys=True, indent=4, ensure_ascii=False).encode('utf-8')
+        allData.sort(key=lambda x: x["likes"]["summary"]["total_count"], reverse=True)
+        likeCount = 0
+        commentCount = 0
+        for post in allData:
+            likeCount = likeCount + int(post["likes"]["summary"]["total_count"])
+            commentCount = commentCount + int(post["comments"]["summary"]["total_count"])
+
+        result = {"post_count": len(allData), "like_count": likeCount, "comment_count": commentCount, "posts": allData[0:100]}
+        result = json.dumps(result, sort_keys=True, indent=4, ensure_ascii=False).encode('utf-8')
         file = open("data.json", 'w')
-        file.write(allData)
+        file.write(result)
         file.close
 
 getData("https://graph.facebook.com/867704779984689/feed?fields=actions,from,message,comments.limit(0).summary(true),likes.limit(0).summary(true)&limit=1000&access_token=CAAWnAJjuBBkBAB6ZAsyDd26F84D5iAZAopuZBv9h4GzC8RGFlwCbJOUtVoQESpBKvSCSPZC6EZC78ZBAkeHZClIkvLWY3dTZB7WZBaeybQN2bVWt8hFrqamvjwizgx66K84Wry4bDoLpWy3MV3spFMgYOPrqMG0olGb7C6Pn4r064hTi1tFODQzLi")
---
+++ js/main.js
@@ -0,0 +1,25 @@
+angular.module("horror", [])
+    .controller("HorrorController", function($scope, $http){
+        var horror = this;
+        var allData;
+        var showCount = 5;
+        $.getJSON("data.json", function(data) {
+            allData = data;
+            horror.data = allData;
+            horror.data.posts = horror.data.posts.slice(0, 5);
+            $("#message").replaceWith("<div id='message'><p style='text-align:center' class='animated fadeIn'>沒有啦，騙你的啦哈哈</p></div>");
+            setTimeout(function(){
+                $("#message").remove();
+                $("#downMessage").show();
+                $scope.$apply();
+                $("#info").show();
+                $(".page").show();
+            }, 1500);
+        });
+
+        horror.showMore = function() {
+            showCount += 10;
+            horror.data.posts = allData.posts.slice(0, showCount);
+            $scope.$apply();
+        };
+    });
