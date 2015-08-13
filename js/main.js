25a8d34187cc9cff04e7698c1b3fc1b340c5dfb0
LarryStanley <kill.xmurderer@gmail.com>
2015-08-13 17:52:16 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 angular.module("horror", [])
     .controller("HorrorController", function($scope, $http){
         var horror = this;
-        var allData;
-        var showCount = 5;
+        horror.allData = [];
+        horror.showCount = 5;
         $.getJSON("data.json", function(data) {
-            allData = data;
+            horror.allData = data;
             horror.data = allData;
             horror.data.posts = horror.data.posts.slice(0, 5);
-            $("#message").replaceWith("<div id='message'><p style='text-align:center' class='animated fadeIn'>沒有啦，騙你的啦哈哈</p></div>");
             setTimeout(function(){
-                $("#message").remove();
-                $("#downMessage").show();
-                $scope.$apply();
-                $("#info").show();
-                $(".page").show();
-            }, 1500);
+                $("#message").replaceWith("<div id='message'><p style='text-align:center' class='animated fadeIn'>沒有啦，騙你的啦哈哈</p></div>");
+                setTimeout(function(){
+                    $("#message").remove();
+                    $("#downMessage").show();
+                    $scope.$apply();
+                    $("#info").show();
+                    $(".page").show();
+                }, 1500);
+            }, 1000);
         });
 
         horror.showMore = function() {
-            showCount += 10;
-            horror.data.posts = allData.posts.slice(0, showCount);
-            //$scope.$apply();
+            horror.showCount += 10;
+            horror.data.posts = horror.allData.posts.slice(0, showCount);
+            console.log(horror.data.posts.length);
         };
     });
