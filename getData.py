b88429d6e9fef2f85c0771195ec1d709d5c9fdda
LarryStanley <kill.xmurderer@gmail.com>
2015-08-15 00:50:21 +0800

    Remove big data.

--- getData.py
+++ getData.py
@@ -25,9 +25,4 @@ def getData(url):
         file.write(result)
         file.close
 
-        allData = json.dumps(allData, sort_keys=True, indent=4, ensure_ascii=False).encode('utf-8')
-        file = open("data.big.json", "w")
-        file.write(allData)
-        file.close
-
 getData("https://graph.facebook.com/867704779984689/feed?fields=actions,from,message,comments.limit(0).summary(true),likes.limit(0).summary(true)&limit=1000&access_token=CAAWnAJjuBBkBAB6ZAsyDd26F84D5iAZAopuZBv9h4GzC8RGFlwCbJOUtVoQESpBKvSCSPZC6EZC78ZBAkeHZClIkvLWY3dTZB7WZBaeybQN2bVWt8hFrqamvjwizgx66K84Wry4bDoLpWy3MV3spFMgYOPrqMG0olGb7C6Pn4r064hTi1tFODQzLi")
