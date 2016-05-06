9816542f6de9b90b4bb6d7d256bf0552813fd621
LarryStanley <kill.xmurderer@gmail.com>
2016-05-06 19:05:40 +0800

    Final update before online.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ angular.module("horror", [])
                     $scope.$apply();
                     $("#info").show();
                     $("#info").addClass("animated fadeIn");
-                    $(".center").append('<p style="text-align:center">Made by <a href="https://goo.gl/7FSJVN" target="_blank">銀行汽車貸款＿吳先生</a></p>');
                     $(".page").show();
                     $("#pageOne").append('<div class="animated fadeIn" id="downMessage" style="position: absolute; left: 50%; bottom:0; padding: 10px 10px 10px 10px;"><div style="position: relative; left: -50%; text-align:center">下拉查看排名<br><i class="fa fa-chevron-down"></i></div></div>')
                 }, 1500);
@@ -22,23 +21,41 @@ angular.module("horror", [])
             horror.showCount = horror.showCount + 10;
             if (horror.showCount > 200)
                 $("#showMoreButton").hide();
+
         };
 
         horror.sortByLike = function() {
+            $("#winnerResults").hide();
+            $("#sortResults").show();
+            $("#showMoreButton").show();
             $("#sortByLikeButton").addClass("active");
             $("#sortByCommentButton").removeClass("active");
+            $("#winnerButton").removeClass("active");
             horror.data.posts.sort(function(a,b){
                 return parseFloat(b.likes.summary.total_count) - parseFloat(a.likes.summary.total_count);
             });
         };
 
         horror.sortByComment = function() {
+            $("#winnerResults").hide();
+            $("#sortResults").show();
+            $("#showMoreButton").show();
             $("#sortByLikeButton").removeClass("active");
             $("#sortByCommentButton").addClass("active");
+            $("#winnerButton").removeClass("active");
             horror.data.posts.sort(function(a,b){
                 return parseFloat(b.comments.summary.total_count) - parseFloat(a.comments.summary.total_count);
             });
         };
+
+        horror.showWinnerList = function() {
+            $("#sortResults").hide();
+            $("#showMoreButton").hide();
+            $("#sortByLikeButton").removeClass("active");
+            $("#sortByCommentButton").removeClass("active");
+            $("#winnerButton").addClass("active");
+            $("#winnerResults").show();
+        }
     });
 $(document).ready(function(){
     $(window).scroll(function (event) {
